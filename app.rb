d6d4f060dc4f72546b653cd6a2e914016fb899b6
Erin Walker <erinb0317@hotmail.com>
2018-03-26 01:33:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,16 @@ class App < Sinatra::Base
   end
 
   post '/team' do
-
+    @team_name = params[:name].to_s 
+    @team_players = [
+      params[:coach],
+      params[:pg],
+      params[:sg],
+      params[:sf],
+      params[:pf],
+      params[:c]
+    ]
+    erb :team 
   end
 
 end
